9d59e6eee1e32098f6b83f4c1aa0301fc189253a
ngishjonathan <balozimwash@gmil.com>
2019-05-10 11:30:57 +0300

    renamed all the files inorder to avoid confusion

---
+++ password.py
@@ -0,0 +1,14 @@
+class Password:
+    
+
+    password_list = []
+
+    def __init__(self,first_name,second_name,user_name,password):
+
+        self.first_name = first_name
+        self.second_name = second_name
+        self.user_name = user_name
+        self.password = password
+ 
+
+    
